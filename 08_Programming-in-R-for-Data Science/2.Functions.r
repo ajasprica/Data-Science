d1d487333abae963e7028923925163a1f08c952f
ajasprica <anto.jasprica@gmail.com>
2016-04-22 19:10:38 +0200

    EdX course notes

---
+++ 08_Programming-in-R-for-Data Science/2.Functions.r
@@ -0,0 +1,30 @@
+### 1. What is the type of the vector x, when x is defined as follows?
+
+x <- c("b", 2,  TRUE)
+class(x)
+
+#[1] "character"
+
+### 2.The vector y is defined as follows:
+  
+y <- c(1, 2, 3.14, 4, 5)
+
+#What will be returned when you show the value y in the console? 
+y
+#[1] 1.00 2.00 3.14 4.00 5.00
+
+### 3.The vector z is defined as follows:
+  
+z <- c(FALSE, TRUE, 2, 3, 4)
+
+#What will be returned when you show the value z in the console?
+z
+#[1] 0 1 2 3 4
+
+### 4.How many elements are TRUE when you create a matrix with the following command?
+
+
+matrix(c(5,4,3,2,1,0)+2,nrow=2)<5
+
+### 5. Which data type in R can hold different type of elements?
+# Data Frame
